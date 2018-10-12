1a0d5f0ae8d43f3744babeec0ff1210b9c1b2c99
Yun-Jan <yt266@njit.edu>
2018-10-11 22:43:58 -0400

    Task: Created csv class

--- public/index.php
+++ public/index.php
@@ -5,20 +5,24 @@
  * Date: 10/11/18
  * Time: 9:58 PM
  */
-main::start();
+main::start("example.csv");
 
 class main {
-    static public function start(){
-        $file= fopen("example.csv", "r");
+    static public function start($filename){
+        $records=csv::getRecords($filename);
+        print_r($records);
+    }
+}
+class csv{
+
+    static public function getRecords($filename){
+        $file= fopen($filename, "r");
         while(! feof($file)){
 
             $record= fgetcsv($file);
             $records[]=$record;
-
         }
-
         fclose($file);
-        print_r($records);
-
+        return $records;
     }
 }
